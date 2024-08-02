130e8013980b30d44236173f6528d705194c6afa
Riccardo Semeraro <150936465+riccardosemeraro@users.noreply.github.com>
2024-08-02 22:07:47 +0000

    authentication

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -8,14 +8,25 @@ const Profile = () => {
     return <div>Loading ...</div>;
   }
 
+
   return (
     isAuthenticated && (
       <div>
         {/* https://auth0.com/docs/manage-users/user-accounts/user-profiles/user-profile-structure#user-profile-attributes */}
+        {/* Object
+                email: "riccardosemer@gmail.com"
+                email_verified: true
+                family_name: "Semeraro"
+                given_name: "Riccardo"
+                name: "Riccardo Semeraro"
+                nickname: "riccardosemer"
+                picture: "https://lh3.googleusercontent.com/a/ACg8ocILwelJWLjwDH0AZ6iOtLYhEV_v5MQHCg17_uobKwWRmPIopIAY=s96-c"
+                sub: "google-oauth2|118391339072684839982"
+                updated_at: "2024-08-02T21:36:59.848Z"
+        */}
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-        <p>{user.address}</p>
       </div>
     )
   );

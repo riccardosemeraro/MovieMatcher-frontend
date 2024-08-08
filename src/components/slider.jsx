29d0772168e9ca0ba31705e10e03ff63ec6faed5
wDaaV <150930604+wDaaV@users.noreply.github.com>
2024-08-08 11:19:33 +0000

    prova 1 tmdb

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -2,6 +2,19 @@ import '../style/slider.css';
 
 function Slider() {
 
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjAxNjQzNjI0ZGY2OTY5NDMwNTRjMzJkNGY3NmI3ZSIsIm5iZiI6MTcyMzExNTUzMS4zNzI1OTgsInN1YiI6IjY2YjRhNTcyZGUzODU5OGY2YTZkMDBmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QjgGj9sr5Euk1A8LEyl4riJw0YthkeujM1mT0rpoiX0'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/movie/now_playing?language=it-IT&page=1', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
     return (
         <>
         <div className="sliderContainer">
@@ -10,12 +23,14 @@ function Slider() {
                 <div className="film">array</div> {/*array di film*/}
             </div>                
         </div>
+        {/*
         <div className="sliderContainer">
             <h2>Consigliati per te</h2>
             <div className="slider">
-                {/*array di film*/}
+                {/*array di film/}
             </div>                
         </div>
+        */}
         </>
     );
 }

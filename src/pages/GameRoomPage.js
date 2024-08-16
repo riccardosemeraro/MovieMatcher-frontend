a85633d5816c2c7b2134daf03b30a237c1d9921d
wDaaV <150930604+wDaaV@users.noreply.github.com>
2024-08-16 17:20:23 +0000

    gameRoom css

--- src/pages/GameRoomPage.js
+++ src/pages/GameRoomPage.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ActionButtons from '../components/actionButton';
+import Button from 'react-bootstrap/Button';
+
+import "../style/gameRoom.css";
 
 function GameRoomPage() {
   return (
-    <div className="explore-page">
-      <main>
-        <h1>Game Room</h1>
-        <div className="explore-content">
-          <ActionButtons />
-        </div>
-      </main>
+    <div className="game-room">
+      <Button variant="primary" className='game-button' onClick={()=>('')}> 
+              <p>Crea Partita</p>
+      </Button>
+      <Button variant="primary" className='game-button' onClick={()=>('')}> 
+              <p>Partecipa a Partita</p>
+      </Button>
+      <Button variant="primary" className='game-button' onClick={()=>('')}> 
+        <p>Match History</p>
+      </Button>
     </div>
   );
 }

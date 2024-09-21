8d7221adbfa0e0e30d71aa03260a49f1fe5e7273
wDaaV <davideverditto@gmail.com>
2024-09-21 18:00:02 +0200

    ultimi fix

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,13 +12,13 @@ function Footer() {
       <div className="desktop-footer">
         <p> © {anno} MovieMatcher - All right reserved </p>
         <p className='smaller'> Informazioni sui Film fornite da <a href = "https://www.themoviedb.org/" >TMDB</a> </p>
-        <p className='smaller'> Progetto sviluppato da <a href='https://github.com/riccardosemeraro'> Riccardo Semeraro </a> - <a href='https://github.com/GaiSer27'> Gaia Sergio </a> - <a href='https://github.com/wDaaV' > Davide Verditto </a> </p>
+        <p className='smaller'> Progetto sviluppato da <a href='https://github.com/riccardosemeraro'> Riccardo Semeraro </a> - <a href='https://github.com/wDaaV' > Davide Verditto </a> </p>
       </div>
 
       <div className='mobile-footer'>
         <p> © {anno} MovieMatcher - All right reserved </p>
         <p className='smaller'> Informazioni sui Film fornite da <a href = "https://www.themoviedb.org/" >TMDB</a> </p>
-        <p className='smaller'> Progetto sviluppato da <br/> <a href='https://github.com/riccardosemeraro'> Riccardo Semeraro </a> - <a href='https://github.com/GaiSer27'> Gaia Sergio </a> - <a href='https://github.com/wDaaV' > Davide Verditto </a> </p>
+        <p className='smaller'> Progetto sviluppato da <br/> <a href='https://github.com/riccardosemeraro'> Riccardo Semeraro </a> - <a href='https://github.com/wDaaV' > Davide Verditto </a> </p>
       </div>
       
     </footer>
